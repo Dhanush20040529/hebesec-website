be5d351801b7f4e0daf8c65561168d9415b0629d
Dhanush20040529 <dhanushdhanush87038@gmail.com>
2025-09-11 14:25:19 +0530

    updated files

--- script.js
+++ script.js
@@ -7,6 +7,21 @@ document.addEventListener("readystatechange",()=>{
 
 const initApp = ()=>{
     const year = document.getElementById("year").textContent = new Date().getFullYear();
+    const menuClsBtn = document.querySelector(".close-menu-btn")
+    const menuOpenBtn = document.querySelector(".open-menu-btn")
+    const menuBtns = document.querySelector(".mobile-nav-btn")
+    
+    menuOpenBtn.addEventListener("click",()=>{
+        menuClsBtn.style.display = "block"
+        menuOpenBtn.style.display = "none"
+        menuBtns.style.transform = "rotateX(0deg)"
+    })
+
+    menuClsBtn.addEventListener("click",()=>{
+        menuClsBtn.style.display = "none"
+        menuOpenBtn.style.display = "block"
+        menuBtns.style.transform = "rotateX(90deg)"
+    })
 
    
 }
